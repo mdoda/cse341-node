2b83a9f7e63a8527eed7cf2334f0d9cd8c44b6d9
mdoda <mateo.doda@outlook.com>
2022-01-14 13:25:08 -0700

    added description in the form(above and beyond)

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,7 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({ title: req.body.title, description: req.body.description});
   res.redirect('/');
 });
 
