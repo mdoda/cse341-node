bf27c20b7426beb291b6e808410239eccc7c24f5
mdoda <mateo.doda@outlook.com>
2022-01-27 10:38:53 -0700

    week04

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+const PORT = process.env.PORT || 5000
+
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
+const cors = require('cors')
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,9 +20,56 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('61f259ba97e6384f8bf6cf75')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+const corsOptions = {
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://mdoda111:Forzamilan123455555@cluster0.rwppj.mongodb.net/shop?retryWrites=true&w=majority";
+     
+
+mongoose
+  .connect(
+     MONGODB_URL,
+     options )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Mateo",
+          email: "mateo@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
